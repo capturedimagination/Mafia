41f5e5aaf6cb29db5b6c42e1db39a9dbd75e1dab
capturedimagination <dawford19@gmail.com>
2025-02-17 22:54:28 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
-const conditionalPlugins: [string, Record<string, any>][] = [];
-
-// @ts-ignore
+const conditionalPlugins = [];
 if (process.env.TEMPO === "true") {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
-  optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
-  },
+  base: "/",
   plugins: [
     react({
       plugins: conditionalPlugins,
@@ -23,13 +18,19 @@ export default defineConfig({
     tempo(),
   ],
   resolve: {
-    preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/],
+      extensions: [".js", ".cjs"],
+    },
+  },
   server: {
-    // @ts-ignore
     allowedHosts: true,
-  }
+  },
 });
